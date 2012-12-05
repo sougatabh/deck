24302d272252fc64094a5f7779340c8f9ea7e673
bhattacharya <sougata@gmail.com>
2012-12-05 10:47:55 +0530

    Indentation fixed

--- project.clj
+++ project.clj
@@ -4,8 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.7.1"]]
-  :dependencies [
-                [org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                 [compojure "1.1.1"]
                 [basil "0.4.0"]
                 [org.clojars.paul/clj-hector "0.2.5"]]
