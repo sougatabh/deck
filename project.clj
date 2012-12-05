cb7827abc4f272a44ab88b0790bcf59b62c832e9
bhattacharya <sougata@gmail.com>
2012-12-05 10:49:44 +0530

    Indentation fixed

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject deck "0.1.0"
-  :description "This is a cassandra admin web tool"
-  :url "http://example.com/FIXME"
+  :description "This is a Cassandra admin web tool"
+  :url "https://github.com/sougatabh/deck"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.7.1"]]
